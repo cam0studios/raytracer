33fbd440ca51729ba4c62f3e5dd6f09bea1de27a
cam0studios <cam321blastoff@gmail.com>
2026-07-26 01:22:49 -0400

    feat: add more wgsl types, set up more internal buffers

--- src/buffers.rs
+++ src/buffers.rs
@@ -3,52 +3,151 @@
 use wgpu::util::DeviceExt;
 
 pub struct BufferManager {
-    pub vars: Vec<f32>, // Vars
+    // static size
+    pub vars: Vars, // Vars
     pub vars_buffer: wgpu::Buffer,
-    // bvh: Vec<f32>, // Bvh[# node]
-    // bvh_buffer: wgpu::Buffer,
-    // scene: Vec<f32>, // Object[# object]
-    // scene_buffer: wgpu::Buffer,
-    // materials: Vec<f32>, // Material[# material]
-    // materials_buffer: wgpu::Buffer,
-    // rays_buffer: wgpu::Buffer, // Ray[# active]
-    // active_rays_buffer: wgpu::Buffer, // bool[# active] OR bitmap
-    // active_ray_counter: wgpu::Buffer, // Indirect
-    // intersections_buffer: wgpu::Buffer, // Intersection[# active]
-    pub output: wgpu::Texture, // Texture[swidth x sheight]
+    pub active_ray_counter: wgpu::Buffer, // Indirect
+    // scene dependent
+    // pub bvh: Vec<f32>, // Bvh[# node]
+    // pub bvh_buffer: wgpu::Buffer,
+    // pub scene: Vec<f32>, // Object[# object]
+    // pub scene_buffer: wgpu::Buffer,
+    // pub materials: Vec<f32>, // Material[# material]
+    // pub materials_buffer: wgpu::Buffer,
+    // size dependent
+    pub rays_buffer: wgpu::Buffer,          // Ray[# active]
+    pub active_rays_buffer: wgpu::Buffer,   // u32[# active / 32] (packed bits)
+    pub intersections_buffer: wgpu::Buffer, // Intersection[# active]
+    pub output_buffer: wgpu::Texture,       // Texture[swidth x sheight]
     pub output_view: wgpu::TextureView,
 }
 impl BufferManager {
     pub fn new(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration) -> Self {
-        let vars: Vec<f32> = vec![config.width as f32, config.height as f32];
+        let size = (config.width, config.height);
+
+        let vars = Vars {
+            size,
+            frame: 0,
+            bounce: 0,
+        };
         let vars_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vars Buffer"),
-            contents: bytemuck::cast_slice(&vars),
+            contents: &vars.to_bytes(),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
 
-        let output = device.create_texture(&wgpu::TextureDescriptor {
+        let active_ray_counter = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Active Ray Counter"),
+            mapped_at_creation: false,
+            size: 16,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::INDIRECT,
+        });
+
+        let size_dependent = Self::get_size_dependent_buffers(device, &size);
+
+        Self {
+            vars,
+            vars_buffer,
+            active_ray_counter,
+            rays_buffer: size_dependent.rays_buffer,
+            active_rays_buffer: size_dependent.active_rays_buffer,
+            intersections_buffer: size_dependent.intersections_buffer,
+            output_buffer: size_dependent.output_buffer,
+            output_view: size_dependent.output_view,
+        }
+    }
+
+    // fn get_scene_dependent_buffers() -> SceneDependentBuffers {}
+
+    fn get_size_dependent_buffers(
+        device: &wgpu::Device,
+        size: &(u32, u32),
+    ) -> SizeDependentBuffers {
+        let rays_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Rays Buffer"),
+            size: (4 * 12 * size.0 * size.1) as u64,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let active_rays_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Active Rays Buffer"),
+            size: (size.0 * size.1 + 31) as u64 / 32,
+            usage: wgpu::BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+
+        let intersections_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Intersections Buffer"),
+            size: (4 * 8 * size.0 * size.1) as u64,
+            usage: wgpu::BufferUsages::STORAGE,
+            mapped_at_creation: false,
+        });
+
+        let output_buffer = device.create_texture(&wgpu::TextureDescriptor {
             label: Some("Output Storage Texture"),
             usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::STORAGE_BINDING,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba32Float,
             mip_level_count: 1,
             sample_count: 1,
-            view_formats: &config.view_formats,
+            view_formats: &[wgpu::TextureFormat::Rgba32Float],
             size: wgpu::Extent3d {
-                width: config.width,
-                height: config.height,
+                width: size.0,
+                height: size.1,
                 depth_or_array_layers: 1,
             },
         });
-        let output_view = output.create_view(&wgpu::TextureViewDescriptor::default());
-        Self {
-            vars,
-            vars_buffer,
-            output,
+        let output_view = output_buffer.create_view(&wgpu::TextureViewDescriptor::default());
+
+        SizeDependentBuffers {
+            rays_buffer,
+            active_rays_buffer,
+            intersections_buffer,
+            output_buffer,
             output_view,
         }
     }
 
-    // pub fn resize(&mut self) {}
+    pub fn write_vars(&self, _device: &wgpu::Device, queue: &wgpu::Queue) {
+        queue.write_buffer(&self.vars_buffer, 0, &self.vars.to_bytes());
+    }
+
+    // pub fn write_scene(&mut self, device: &wgpu::Device) {}
+
+    pub fn resize(&mut self, device: &wgpu::Device, size: &(u32, u32)) {
+        let size_dependent = BufferManager::get_size_dependent_buffers(device, size);
+        self.rays_buffer = size_dependent.rays_buffer;
+        self.active_rays_buffer = size_dependent.active_rays_buffer;
+        self.intersections_buffer = size_dependent.intersections_buffer;
+        self.output_buffer = size_dependent.output_buffer;
+        self.output_view = size_dependent.output_view;
+    }
+}
+
+// struct SceneDependentBuffers {}
+struct SizeDependentBuffers {
+    rays_buffer: wgpu::Buffer,
+    active_rays_buffer: wgpu::Buffer,
+    intersections_buffer: wgpu::Buffer,
+    output_buffer: wgpu::Texture,
+    output_view: wgpu::TextureView,
+}
+
+pub struct Vars {
+    size: (u32, u32),
+    frame: u32,
+    bounce: u32,
+}
+impl Vars {
+    pub fn to_bytes(&self) -> [u8; 16] {
+        let size_slice_binding = [self.size.0, self.size.1];
+        let frame_slice_binding = [self.frame];
+        let bounce_slice_binding = [self.bounce];
+        let mut ret = [0 as u8; 16];
+        ret[..8].copy_from_slice(bytemuck::cast_slice(&size_slice_binding));
+        ret[8..12].copy_from_slice(bytemuck::cast_slice(&frame_slice_binding));
+        ret[12..16].copy_from_slice(bytemuck::cast_slice(&bounce_slice_binding));
+        ret
+    }
 }
